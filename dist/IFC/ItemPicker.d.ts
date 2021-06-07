374c404fc75e0ac552425cdb20f0599678709ed7
Antonio <antoniogviegas@hotmail.com>
2021-06-08 00:32:25 +0200

    Delete dist content

--- dist/IFC/ItemPicker.d.ts
+++
@@ -1,7 +0,0 @@
-import { BufferGeometry, Intersection } from "three";
-import { DisplayManager } from "./DisplayManager";
-export declare class ItemPicker {
-    private display;
-    constructor(displayManager: DisplayManager);
-    pickItem(items: Intersection[], geometry: BufferGeometry, pickTransparent?: boolean): Intersection | null | undefined;
-}
--- dist/IFC/Shaders.d.ts
+++
@@ -1,3 +0,0 @@
-import { Shader } from 'three';
-export declare function OpaqueShader(shader: Shader): void;
-export declare function TransparentShader(shader: Shader): void;
